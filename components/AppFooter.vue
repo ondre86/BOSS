f2d85f9f9859f44ab9b0a966e832f2e8890873cb
ondre86 <ondre86@gmail.com>
2025-01-12 11:07:05 -0600

    ALPHA 0.2 (links)

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,10 +6,10 @@
             </a>
         </nav>
         <div>
-            <a href="">
+            <NuxtLink to="https://www.google.com/maps/place/5790+Crowder+Blvd+Ste+E,+New+Orleans,+LA+70127/@30.0281244,-89.9967336,17z/data=!3m1!4b1!4m6!3m5!1s0x889e027bb876935b:0x97ca628b597ff069!8m2!3d30.0281244!4d-89.9941587!16s%2Fg%2F11scv0fd7b?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">
                 5790 Crowder Blvd. Suite E<br>
                 New Orleans, LA, 70807
-            </a>
+            </NuxtLink>
         </div>
     </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,9 @@
             <div class="flex flex-col gap-8 text-center">
                 <div class="flex flex-col gap-4">
                     <h1 class="text-4xl">Coming Soon...</h1>
-                    <h2 class="text-2xl">5790 Crowder Blvd. Suite E, New Orleans, LA</h2>
+                    <h2 class="text-2xl">
+                      <NuxtLink to="https://www.google.com/maps/place/5790+Crowder+Blvd+Ste+E,+New+Orleans,+LA+70127/@30.0281244,-89.9967336,17z/data=!3m1!4b1!4m6!3m5!1s0x889e027bb876935b:0x97ca628b597ff069!8m2!3d30.0281244!4d-89.9941587!16s%2Fg%2F11scv0fd7b?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">5790 Crowder Blvd. Suite E, New Orleans, LA</NuxtLink>
+                    </h2>
                 </div>
                 <p class="max-w-prose">
                     BOSS provides comprehensive business support services, including professional printing and shipping, copy, fax, and scan solutions, certified notary services, expert tax preparation, and administrative assistance. <br><br>
